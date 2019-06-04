5ad0fe887c3762b69de66e816900f4f51058c4dc
afhare <alexandra.f.hare@gmail.com>
2019-06-04 10:47:26 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_reader :height
+    attr_accessor :name, :weight
+
+    @@all = []
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
