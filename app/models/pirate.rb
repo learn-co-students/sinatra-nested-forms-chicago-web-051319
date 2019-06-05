38e557700fa0b5cc791d1fb9e8a9ea50186fbb6c
Derek Townsend <derekjavon2@gmail.com>
2019-06-05 13:17:57 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  def initialize(pirate_info)
+    @name = pirate_info[:name]
+    @weight = pirate_info[:weight]
+    @height = pirate_info[:height]
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+  def initialize(ship_info)
+    @name = ship_info[:name]
+    @type = ship_info[:type]
+    @booty = ship_info[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
